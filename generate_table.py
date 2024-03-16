4a89dc9edd4f3892243f7e4875dd9022a9f05fce
Mikhail Loykonen <mr.loykonen@mail.ru>
2024-03-17 00:05:55 +0300

    add calculate mean in generate_table

--- generate_table.py
+++ generate_table.py
@@ -7,8 +7,9 @@ from os import path
 
 @click.command()
 @click.option("--filepath", "filepath", required=True, help="Path to jsonl file", type=click.Path(exists=True))
+@click.option("--mean", "mean", is_flag=True, required=False, default=False, help="Calculate the average values of the attributes for each record")
 @click.option("--output", "output", required=False, default="output.csv", help="Name of output file")
-def generate_csv_features_table(filepath, output):
+def generate_csv_features_table(filepath, mean, output):
     if filepath.rsplit('.', 1)[-1].lower() != 'jsonl':
         print("Incorrect file type. Required .jsonl")
         return
@@ -18,6 +19,8 @@ def generate_csv_features_table(filepath, output):
             data = json.loads(line)
             features = np.load(path.normpath(filepath + '/../' + data['tensor']))[0]
             features_df = pd.DataFrame(features).T
+            if mean:
+                features_df = features_df.mean(axis=0).to_frame().T
             features_df['label'] = data['label']
             features_df['emotion'] = data['emotion']
             df = pd.concat([df, features_df], axis=0)
