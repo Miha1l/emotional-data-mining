e66ee327a4cd4610e0af194c0919a27df3672e5e
Mikhail Loykonen <mr.loykonen@mail.ru>
2025-03-13 17:46:03 +0300

    update script for generate triplets

--- scripts/create_triplet_dataset.py
+++ scripts/create_triplet_dataset.py
@@ -11,17 +11,18 @@ anchors_df = df.groupby(['label']).head(N_SAMPLES_PER_CLASS).reset_index(drop=Tr
 triplets = []
 
 for _, anchor in anchors_df.iterrows():
-    pos_sample = df.loc[(df["label"] == anchor["label"]) & (df["id"] != anchor["id"])].sample(1)
-    neg_sample = df.loc[(df["label"] != anchor["label"]) & (df["id"] != anchor["id"])].sample(1)
+    pos_samples = df.loc[(df["label"] == anchor["label"]) & (df["id"] != anchor["id"])].sample(3)
+    neg_samples = df.loc[df["label"] != anchor["label"]].groupby('label').sample(1)
 
-    triplets.append({
-        "anchors": anchor["audio_path"],
-        "positives": pos_sample["audio_path"].iloc[0],
-        "negatives": neg_sample["audio_path"].iloc[0],
-        "anchors_labels": anchor["label"],
-        "positives_labels": pos_sample["label"].iloc[0],
-        "negatives_labels": neg_sample["label"].iloc[0],
-    })
+    for i in range(3):
+        triplets.append({
+            "anchor": anchor["audio_path"],
+            "positive": pos_samples["audio_path"].iloc[i],
+            "negative": neg_samples["audio_path"].iloc[i],
+            "anchor_label": anchor["label"],
+            "positive_label": pos_samples["label"].iloc[i],
+            "negative_label": neg_samples["label"].iloc[i],
+        })
 
 triplet_df = pd.DataFrame(triplets)
 triplet_df.to_csv(f"../data/triplets_{len(triplets)}.csv", index=False)
