a86ab1b91bfb8ad0e1607798477a9d53a339344b
Mikhail Loykonen <mr.loykonen@mail.ru>
2025-04-10 16:18:36 +0300

    upd generate datasets scripts for create binary datasets

--- scripts/create_triplet_dataset.py
+++ scripts/create_triplet_dataset.py
@@ -1,28 +1,34 @@
 import pandas as pd
 
 N_LABELS = 4
-N_SAMPLES_PER_CLASS = 100
-N_SAMPLES = N_SAMPLES_PER_CLASS * N_LABELS
+N_SAMPLES_PER_CLASS = 15
+# N_SAMPLES = N_SAMPLES_PER_CLASS * N_LABELS
 
-df = pd.read_csv('../data/crowd_train_2000.csv')
 
-anchors_df = df.groupby(['label']).head(N_SAMPLES_PER_CLASS).reset_index(drop=True)
+def create_triplet_dataset(filepath, n_labels, n_samples_per_class):
+    df = pd.read_csv(filepath)
 
-triplets = []
+    anchors_df = df.groupby(['label']).head(n_samples_per_class).reset_index(drop=True)
 
-for _, anchor in anchors_df.iterrows():
-    pos_samples = df.loc[(df["label"] == anchor["label"]) & (df["id"] != anchor["id"])].sample(3)
-    neg_samples = df.loc[df["label"] != anchor["label"]].groupby('label').sample(1)
+    triplets = []
+    for _, anchor in anchors_df.iterrows():
+        pos_samples = df.loc[(df["label"] == anchor["label"]) & (df["id"] != anchor["id"])].sample(3)
+        neg_samples = df.loc[df["label"] != anchor["label"]].groupby('label').sample(1)
 
-    for i in range(3):
-        triplets.append({
-            "anchor": anchor["audio_path"],
-            "positive": pos_samples["audio_path"].iloc[i],
-            "negative": neg_samples["audio_path"].iloc[i],
-            "anchor_label": anchor["label"],
-            "positive_label": pos_samples["label"].iloc[i],
-            "negative_label": neg_samples["label"].iloc[i],
-        })
+        for i in range(n_labels - 1):
+            triplets.append({
+                "anchor": anchor["audio_path"],
+                "positive": pos_samples["audio_path"].iloc[i],
+                "negative": neg_samples["audio_path"].iloc[i],
+                "anchor_label": anchor["label"],
+                "positive_label": pos_samples["label"].iloc[i],
+                "negative_label": neg_samples["label"].iloc[i],
+            })
 
-triplet_df = pd.DataFrame(triplets)
-triplet_df.to_csv(f"../data/triplets_{len(triplets)}.csv", index=False)
+    triplet_df = pd.DataFrame(triplets)
+    triplet_df.to_csv(f"../data/triplets_{n_labels}c_{len(triplets)}.csv", index=False)
+
+
+if __name__ == '__main__':
+    # create_triplet_dataset('../data/crowd_train_2000.csv', N_LABELS, N_SAMPLES_PER_CLASS)
+    create_triplet_dataset('../data/crowd_train_bin_4000.csv', 2, 300)
--- scripts/get_balanced_dataset.py
+++ scripts/get_balanced_dataset.py
@@ -9,7 +9,20 @@ def create_balanced_df(dataset_path, n_samples, save_path):
     print(f"Суммарное время аудио: {datetime.timedelta(seconds=df_part['duration'].sum())}")
 
 
+def create_bin_balanced_df(dataset_path, n_samples, save_path):
+    df = pd.read_csv(dataset_path)
+    df = df.loc[(df["emotion"] == 'positive') | (df["emotion"] == 'sad')]
+
+    print(df[['emotion']].groupby(['emotion']).value_counts())
+
+    df_part = df.groupby(['emotion']).head(n_samples).reset_index(drop=True)
+    df_part.to_csv(save_path, index=False)
+
+    print(f"Суммарное время аудио: {datetime.timedelta(seconds=df_part['duration'].sum())}")
+
+
 if __name__ == '__main__':
-    n_samples = 750
+    n_samples = 4000
     dataset_name = 'crowd_train'
-    create_balanced_df(f'../data/{dataset_name}.csv', n_samples, f'../data/{dataset_name}_{n_samples}.csv')
+    # create_balanced_df(f'../data/{dataset_name}.csv', n_samples, f'../data/{dataset_name}_{n_samples}.csv')
+    create_bin_balanced_df(f'../data/{dataset_name}.csv', n_samples, f'../data/{dataset_name}_bin_{n_samples}.csv')
